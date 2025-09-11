b01c6e08e3a6a667f5b6afc30d649b7ec60333a6
de-gupta <de-gupta>
2025-09-11 10:07:24 +0200

    _filterPropertyAndAdditionalFilter: active filter static factory method
    Took 3 minutes

--- src/main/java/de/gupta/clean/crud/template/useCases/query/specification/domain/model/ActiveFilterSpecification.java
+++ src/main/java/de/gupta/clean/crud/template/useCases/query/specification/domain/model/ActiveFilterSpecification.java
@@ -1,5 +1,11 @@
 package de.gupta.clean.crud.template.useCases.query.specification.domain.model;
 
-public record ActiveFilterSpecification() implements LeafFilterSpecification
+public final class ActiveFilterSpecification implements LeafFilterSpecification
 {
+	private static final ActiveFilterSpecification INSTANCE = new ActiveFilterSpecification();
+
+	public static ActiveFilterSpecification instance()
+	{
+		return INSTANCE;
+	}
 }
