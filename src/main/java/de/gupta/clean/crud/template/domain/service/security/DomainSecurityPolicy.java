2822852d3c2b54074a5de48c6b7b7c0acced2f8f
de-gupta <de-gupta>
2025-09-26 09:55:34 +0200

    static factories for domain security policy
    Took 5 minutes

--- src/main/java/de/gupta/clean/crud/template/domain/service/security/DomainSecurityPolicy.java
+++ src/main/java/de/gupta/clean/crud/template/domain/service/security/DomainSecurityPolicy.java
@@ -3,5 +3,15 @@ package de.gupta.clean.crud.template.domain.service.security;
 @FunctionalInterface
 public interface DomainSecurityPolicy<DomainModel>
 {
+	static <DomainModel> DomainSecurityPolicy<DomainModel> allowing()
+	{
+		return _ -> true;
+	}
+
+	static <DomainModel> DomainSecurityPolicy<DomainModel> denying()
+	{
+		return _ -> false;
+	}
+
 	boolean isAccessAllowed(final DomainModel domainModel);
 }
