e3c682d58eb367c99b370f6ccd2e9af54ca5063d
de-gupta <de-gupta>
2026-03-23 13:58:44 +0100

    removed older deprecated crud/all endpoints

--- src/main/java/de/gupta/clean/crud/template/domain/service/existence/ResourceExistenceDetectionService.java
+++
@@ -1,8 +0,0 @@
-package de.gupta.clean.crud.template.domain.service.existence;
-
-@FunctionalInterface
-@Deprecated
-public interface ResourceExistenceDetectionService<DomainModel>
-{
-	boolean existsByModel(final DomainModel model);
-}
